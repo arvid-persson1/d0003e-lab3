e5a8119a5c6e5278aa4e25b7fe306958312b2b7b
arvid-persson1 <a.persson04@gmail.com>
2025-02-07 09:57:06 +0100

    fixed yield using wrong queue

--- src/tinythreads.c
+++ src/tinythreads.c
@@ -40,14 +40,8 @@ static void initialize(void) {
 }
 
 static void enqueue(const Thread p, Thread * const queue) {
-    if (*queue == NULL) {
-        p->next = NULL;
-        *queue = p;
-    } else {
-        Thread q = *queue;
-        *queue = p;
-        p->next = q;
-    }
+    p->next = *queue;
+    *queue = p;
 }
 
 static Thread dequeue(Thread * const queue) {
@@ -104,7 +98,7 @@ void spawn(void (* const function)(const int), const int arg) {
 }
 
 void yield(void) {
-    Thread t = dequeue(&freeQ);
+    Thread t = dequeue(&readyQ);
     enqueue(current, &readyQ);
     dispatch(t);
 }
